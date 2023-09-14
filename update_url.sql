e4f6c14f9e2f60df3f915331a80035ece6ee5d6e
Patrick Steenks <patrick@keytoe.nl>
2023-09-14 21:59:24 +0200

    Updated script

--- update_url.sql
+++ update_url.sql
@@ -2,10 +2,10 @@
 SET @old_url = 'your_old_url';
 SET @new_url = 'your_new_url';
 
-UPDATE wp_options SET option_value = replace(option_value, '@old_url', '@new_url') WHERE option_name = 'home' OR option_name = 'siteurl';
+UPDATE mvl_options SET option_value = replace(option_value, @old_url, @new_url) WHERE option_name = 'home' OR option_name = 'siteurl';
 
-UPDATE wp_posts SET guid = replace(guid, '@old_url','@new_url');
+UPDATE mvl_posts SET guid = replace(guid, @old_url, @new_url);
 
-UPDATE wp_posts SET post_content = replace(post_content, '@old_url', '@new_url');
+UPDATE mvl_posts SET post_content = replace(post_content, @old_url, @new_url);
 
-UPDATE wp_postmeta SET meta_value = replace(meta_value,'@old_url','@new_url');
+UPDATE mvl_postmeta SET meta_value = replace(meta_value, @old_url, @new_url);
